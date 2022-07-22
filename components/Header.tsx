9987846bafad2d6160b142f94d5b42d18c4ecc97
Odun Freeman <bornduringwar@gmail.com>
2022-07-22 17:02:59 -0400

    installed tailwind / made Header component

---
+++ components/Header.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next';
+
+export const Header: NextPage = () => {
+  return (
+    <header className="border-8 border-red-600">
+      <h1>Cleveland Stargazer Society</h1>
+    </header>
+  );
+};
+export default Header;
